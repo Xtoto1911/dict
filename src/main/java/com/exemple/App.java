f008313103ee539d5e177c1368184ddca7ae14c3
Xtoto1911 <114175559+Xtoto1911@users.noreply.github.com>
2025-08-22 07:19:59 +0300

    applicationContext

--- src/main/java/com/exemple/App.java
+++ src/main/java/com/exemple/App.java
@@ -1,10 +1,13 @@
 package com.exemple;
 
 import com.exemple.menu.DictionaryMenu;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
     public static void main(String[] args) {
-        DictionaryMenu dictionaryMenu = new DictionaryMenu();
+        ApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+        DictionaryMenu dictionaryMenu = context.getBean("dictionaryMenu", DictionaryMenu.class);
         dictionaryMenu.run();
     }
 }
--- src/main/java/com/exemple/dictionary/LatinDictionary.java
+++ src/main/java/com/exemple/dictionary/LatinDictionary.java
@@ -2,7 +2,6 @@ package com.exemple.dictionary;
 
 import com.exemple.utils.StringUtils;
 
-import java.io.IOException;
 
 public class LatinDictionary extends BaseDictionary {
 
